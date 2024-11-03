753eb480da1986692ced8f2f618231675c32a76c
DiStail88 <distail888@gmail.com>
2024-11-03 21:48:47 +0300

    Исправил недочеты

--- hw-3.js
+++ hw-3.js
@@ -1,7 +1,7 @@
 // Задание 1
 
 let password = 'пароль';
-let enter = String(prompt('Введите пароль'));
+let enter = prompt('Введите пароль');
 
 if (enter === password) {
     console.log('Пароль введен верно');
@@ -43,53 +43,29 @@ let monthNumber = Number(prompt('Введите номер месяца'));
 
 switch (monthNumber) {
     case 1:
-        console.log('Зима');
-        break;
-    
     case 2:
+    case 12:
         console.log('Зима');
         break;
 
     case 3:
-        console.log('Весна');
-        break;
-
     case 4:
-        console.log('Весна');
-        break;
-
     case 5:
         console.log('Весна');
         break;
 
     case 6:
+    case 7:
+    case 8:
         console.log('Лето');
         break;        
 
-    case 7:
-        console.log('Лето');
-        break;
-        
-    case 8:
-        console.log('Лето');
-        break;
-
     case 9:
-        console.log('Осень');
-        break;
-
     case 10:
-        console.log('Осень');
-        break;
-
     case 11:
         console.log('Осень');
         break;
-
-    case 12:
-        console.log('Зима');
-        break;    
-
+   
     default:
         console.log('Вы ввели что-то не то!');
         break;
